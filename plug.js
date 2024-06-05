e0891cc6a279f69afbe8c4cb23e38fc46eb1da43
loite <51319329+loite@users.noreply.github.com>
2024-06-05 21:35:30 +0800

    1
    1

---
+++ plug.js
@@ -0,0 +1,8 @@
+<script>
+    let d = document.getElementById("header");
+    let node = document.createElement('a');
+    node.href="https://github.com/loite/loite.github.io";
+    let text = document.createTextNode("loiteBlog");
+    node.appendChild(text);
+    d.appendChild(node);
+</script>
